785d9c8eadeae3b428b8accacdde46a1e460ff53
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-17 21:23:22 +0600

    1. Создана сущность для контактов 2. Создана форма 3. Выровнены стили

--- src/Sibers/BlogBundle/Controller/ContactController.php
+++ src/Sibers/BlogBundle/Controller/ContactController.php
@@ -11,12 +11,35 @@ namespace Sibers\BlogBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Sibers\BlogBundle\Entity\Enquiry;
+use Sibers\BlogBundle\Form\EnquiryType;
 
 
 class ContactController extends Controller
 {
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('main/contact.html.twig');
+//        return $this->render('main/contact.html.twig');
+
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('main/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
