c9137a19511f5294cbc832d88b33695b12d169dc
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-18 21:58:44 +0600

    Немного изменена фикстура

--- src/Sibers/BlogBundle/DataFixtures/ORM/BlogFixtures.php
+++ src/Sibers/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -14,11 +14,11 @@ class BlogFixtures  extends Fixture implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $blog1 = new Blog();
-        $blog1->setTitle('A day with Symfony2');
+        $blog1->setTitle('A day with Symfony3');
         $blog1->setBlog('Lorem ipsum dolor sit amet, consectetur adipiscing eletra electrify denim vel ports.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut velocity magna. Etiam vehicula nunc non leo hendrerit commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras el mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras elementum molestie vestibulum. Morbi id quam nisl. Praesent hendrerit, orci sed elementum lobortis, justo mauris lacinia libero, non facilisis purus ipsum non mi. Aliquam sollicitudin, augue id vestibulum iaculis, sem lectus convallis nunc, vel scelerisque lorem tortor ac nunc. Donec pharetra eleifend enim vel porta.');
         $blog1->setImage('beach.jpg');
         $blog1->setAuthor('dsyph3r');
-        $blog1->setTags('symfony2, php, paradise, symblog');
+        $blog1->setTags('symfony3, php, paradise, symblog');
         $blog1->setCreated(new \DateTime());
         $blog1->setUpdated($blog1->getCreated());
         $manager->persist($blog1);
