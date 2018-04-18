354b409cfe2317bc2915675b8b65b3a360f75e6f
Valeria <valeria.antonia001@gmail.com>
2018-04-18 17:38:41 +0700

    Создан контроллер для комментариев, созданы шалоны для формы добавления комментариев

---
+++ src/Sibers/BlogBundle/Controller/CommentsControler.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace Sibers\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sibers\BlogBundle\Entity\Comment;
+use Sibers\BlogBundle\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Comment controller.
+ */
+
+class CommentsControler extends Controller
+{
+    public function newAction($blog_id)
+    {
+        $blog = $this->getBlog($blog_id);
+
+        $comment = new Comment();
+        $comment->setBlog($blog);
+        $form   = $this->createForm(CommentType::class, $comment);
+
+        return $this->render('comment/form.html.twig', array(
+            'comment' => $comment,
+            'form'   => $form->createView()
+        ));
+    }
+
+
+    public function createAction(Request $request, $blog_id)
+    {
+        $blog = $this->getBlog($blog_id);
+
+        $comment  = new Comment();
+        $comment->setBlog($blog);
+        $form    = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+            // TODO: Persist the comment entity
+
+            return $this->redirect($this->generateUrl('SibersBlogBundle_blog_show', array(
+                    'id' => $comment->getBlog()->getId())) .
+                '#comment-' . $comment->getId()
+            );
+        }
+
+        return $this->render('comment/create.html.twig', array(
+            'comment' => $comment,
+            'form'    => $form->createView()
+        ));
+    }
+
+    protected function getBlog($blog_id)
+    {
+        $em = $this->getDoctrine()
+            ->getManager();
+
+        $blog = $em->getRepository('SibersBlogBundle:Blog')->find($blog_id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $blog;
+    }
+
+
+}
